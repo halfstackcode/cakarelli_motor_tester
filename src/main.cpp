66049e5592fcb3942c0916d8af3ba775679b91aa
Artis Avotins <artis.avotins@gmail.com>
2022-11-15 17:04:46 +0200

    initial

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#include "ESP32_Servo.h"
+#include "ESC.h"
+
+#define ESC_PIN 9
+
+/* This example shows how to use an ESC controller that can go
+ * only forward
+ */
+ESC esc;
+
+void setup() 
+{ 
+    esc.attach(ESC_PIN);
+    esc.setSpeed(30);
+    vTaskDelay(1000);
+    esc.setSpeed(0);
+    vTaskDelay(1000);
+    esc.setSpeed(35);
+    vTaskDelay(1000);
+    esc.setSpeed(0);  
+} 
+
+void loop() 
+{ 
+
+} 
